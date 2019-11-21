afdde960548b8a6a395d81d5f156f7ad10ceb60c
Vikranth Srivatsa <rama22446@gmail.com>
2019-11-21 03:22:35 -0800

    Add Basic lp solver with planned constraints

--- lp_solver.py
+++ lp_solver.py
@@ -22,3 +22,55 @@ def build_graph_given(num_of_locations, num_houses, list_locations, list_houses,
             G.add_edge(loc, other_loc, weight=adjacency_matrix[i][j])
     return G, list_locations, list_houses, starting_car_location
 
+
+def solve(graph, list_locations, list_houses, starting_car_location):
+    shortest_path_all_pairs = nx.all_pairs_dijkstra_path_length(graph)  # Shortest path between all vertices
+    TA = list_houses  # Vertex of the location of the TA at that spot
+
+    m = Model(sense=MINIMIZE, solver_name=CBC)  # use GRB for Gurobi
+    # variable that represents if the car takes the route
+    x = [m.add_var(name='car_taken_{}'.format(u, v), var_type=BINARY) for (u, v) in G.edges()]
+    
+    for loc in list_locations:
+        incident_outgoing_edges = graph.in_edges(loc) + graph.out_edges(loc)
+        m += xsum(x[u][v] for (u, v) in incident_outgoing_edges) % 2 == 0, 'verify_even_car_routes_{}'.format(loc)
+    
+    T = {}
+    for house in list_houses:
+        for loc in list_locations:
+            T[house][loc] = m.add_var(
+                name='ta_dropped_off_{}'.format(house), var_type=BINARY)
+
+        m += xsum(T[house]) == 1 # Each ta must be dropped off
+        
+
+    w = [m.add_var(name='flow_in_{}'.format(house), var_type=BINARY) if house != starting_car_location else 1
+          for house in list_houses]
+
+    for loc in list_locations:
+        incoming_edges = graph.in_edges(loc)
+        m += any(x[u][v] * w[v] for (u, v) in incoming_edges) - w_i[i] == 0, 'verify_connected_loc_{}'.format(loc)
+
+    car_travel = 2/3 * xsum(x[u][v] * d['weight'] * w[v] for (u, v, d) in graph.edges(data=True))
+    
+    ta_travel = 1 * xsum(xsum(T[loc][house] * shortest_path_all_pairs[loc][house] * w[loc]
+                              for loc in list_locations) for ta in list_of_homes)
+
+    m.objective = car_travel + ta_travel
+
+    m.max_gap = 0.02
+    status = m.optimize(max_seconds=300)
+    if status == OptimizationStatus.OPTIMAL:
+        print('optimal solution cost {} found'.format(m.objective_value))
+    elif status == OptimizationStatus.FEASIBLE:
+        print('sol.cost {} found, best possible: {}'.format(
+            m.objective_value, m.objective_bound))
+    elif status == OptimizationStatus.NO_SOLUTION_FOUND:
+        print('no feasible solution found, lower bound is: {}'.format(
+            m.objective_bound))
+    if status == OptimizationStatus.OPTIMAL or status == OptimizationStatus.FEASIBLE:
+        print('solution:')
+        for v in m.vars:
+        if abs(v.x) > 1e-6:  # only printing non-zeros
+            print('{} : {}'.format(v.name, v.x))
+    return m.objective_value, m.objective_bound, x, T
