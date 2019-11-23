632da4a7ef160429c08a9db3f8bdf09c9bdc9b3a
Rohan Koodli <rovik05@gmail.com>
2019-11-22 18:57:34 -0800

    Create independent hourglasses

--- generate_hourglass.py
+++ generate_hourglass.py
@@ -6,14 +6,14 @@ import matplotlib.pyplot as plt
 N = 50
 index = 0
 
-name_nodes = list(range(1, N + 1))
+name_nodes = list(range( N))
 def initialize_graph():
     G = nx.DiGraph()
     index = 0
     G.add_node(name_nodes[index])
     index = 1
     pivot = 0
-    return G, index, pivot
+    return G, pivot, index
 
 def add_hourglass(G, pivot, index):
     G.add_nodes_from(name_nodes[index:index+5])
@@ -29,16 +29,41 @@ def add_hourglass(G, pivot, index):
     G.add_edge(index + 4, index + 3, weight=1)
 
     index += 5
-
-
-    G.add_edge(pivot, pivot+1)
     pivot += 5
+    return pivot, index
 
-G, index, pivot = initialize_graph()
-add_hourglass(G, index, pivot)
+def add_walk_home(G, pivot, index):
+    G.add_nodes_from(name_nodes[index:index+5])
+
+    for i in range(1, 5):
+        G.add_edge(index, index + i, weight=1)
+        G.add_edge(index + i, index, weight=1)
+
+    G.add_edge(index + 1, index + 2, weight=1.5)
+    G.add_edge(index + 2, index + 1, weight=1.5)
+
+    G.add_edge(index + 3, index + 4, weight=1.5)
+    G.add_edge(index + 4, index + 3, weight=1.5)
+
+    index += 5
+    pivot += 5
+    return pivot, index
+
+G, pivot, index = initialize_graph()
+pivot, index = add_hourglass(G, pivot, index)
+pivot, index = add_hourglass(G, pivot, index)
+
+G.add_edge(0, 1, weight=1)
+G.add_edge(1, 0, weight=1)
+# G.add_edge(1, 6, weight=1)
+# G.add_edge(6, 11, weight=1)
 
 a = nx.adjacency_matrix(G)
-print(a.todense())
+a.toarray()
+print((a.toarray()))
+print(type(a.toarray().tolist()))
 
-nx.draw(G, with_labels=True)
-plt.show()
+print(pivot, index)
+
+nx.draw_shell(G, with_labels=True)
+# plt.show()
