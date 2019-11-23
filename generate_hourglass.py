b5207759949d26ff17905aa1d9ad731680ae82e5
Rohan Koodli <rovik05@gmail.com>
2019-11-22 18:27:10 -0800

    Functions and plotting

--- generate_hourglass.py
+++ generate_hourglass.py
@@ -1,34 +1,44 @@
 import networkx as nx
 import scipy as sp
 import random
+import matplotlib.pyplot as plt
 
 N = 50
 index = 0
 
 name_nodes = list(range(1, N + 1))
-G = nx.DiGraph()
+def initialize_graph():
+    G = nx.DiGraph()
+    index = 0
+    G.add_node(name_nodes[index])
+    index = 1
+    pivot = 0
+    return G, index, pivot
 
-G.add_node(name_nodes[index])
-index += 1
-pivot = 0
+def add_hourglass(G, pivot, index):
+    G.add_nodes_from(name_nodes[index:index+5])
+
+    for i in range(1, 5):
+        G.add_edge(index, index + i, weight=100)
+        G.add_edge(index + i, index, weight=100)
+
+    G.add_edge(index + 1, index + 2, weight=1)
+    G.add_edge(index + 2, index + 1, weight=1)
+
+    G.add_edge(index + 3, index + 4, weight=1)
+    G.add_edge(index + 4, index + 3, weight=1)
+
+    index += 5
 
 
-G.add_nodes_from(name_nodes[index:index+5])
+    G.add_edge(pivot, pivot+1)
+    pivot += 5
 
-for i in range(1, 5):
-    G.add_edge(index, index + i, weight=100)
-    G.add_edge(index + i, index, weight=100)
-
-G.add_edge(index + 1, index + 2, weight=1)
-G.add_edge(index + 2, index + 1, weight=1)
-
-G.add_edge(index + 3, index + 4, weight=1)
-G.add_edge(index + 4, index + 3, weight=1)
-
-index += 5
-
-
-G.add_edge(pivot, pivot+1)
+G, index, pivot = initialize_graph()
+add_hourglass(G, index, pivot)
 
 a = nx.adjacency_matrix(G)
 print(a.todense())
+
+nx.draw(G, with_labels=True)
+plt.show()
