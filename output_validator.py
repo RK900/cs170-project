e682c4d9d0f44cebf64d8321e4df5a6af5df1074
Vikranth <rama22446@gmail.com>
2019-11-27 11:22:49 -0800

    re run diffed inputs

--- output_validator.py
+++ output_validator.py
@@ -111,10 +111,10 @@ def tests(input_data, output_data, params=[]):
 
 
 if __name__ == '__main__':
-    input_file, output_file = 'phase2_inputs', 'phase2_outputs'
+    input_file, output_file = 'phase2_inputs/71_200.in', 'phase2_outputs/71_200.out'
     input_validator.RANGE_OF_INPUT_SIZES.append(6)
     input_validator.VALID_FILENAMES.append(input_file)
-    validate_all_outputs(input_file, output_file, params=[])
+    validate_output(input_file, output_file, params=[])
 
     # parser = argparse.ArgumentParser(description='Parsing arguments')
     # parser.add_argument('--all', action='store_true', help='If specified, the output validator is run on all files in the output directory. Else, it is run on just the given output file')
