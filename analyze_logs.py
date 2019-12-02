632c50f4bf12e2bec0d7ae6c1a7cc692f7d76ce5
Vikranth <rama22446@gmail.com>
2019-12-02 11:47:40 -0800

    Update analyze logs to allow for args

--- analyze_logs.py
+++ analyze_logs.py
@@ -1,11 +1,18 @@
 import os
+import sys
 lst = []
 for item in os.listdir('phase2_log/'):
     with open('phase2_log/' + item,'r') as f:
         lst.append(f.readlines() + [item])
 new_lst = [[float(item[0].split(": ")[1].strip()),float(item[1].split(": ")[1].strip()), item[2]] for item in lst]
 err = 0.001
+if len(sys.argv) >= 2:
+    err = float(sys.argv[1])
+print("error bound {}".format(err))
+i = 0
 for item in new_lst:
     diff = abs((item[1] - item[0])/item[1])
     if diff > err:
+        i += 1
         print(item[2], item[1], item[0], diff)
+print("{} items with error greater than {}".format(i, err))
