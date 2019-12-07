effad91d351e5d258df901ea73b83fa08bb1ba05
Vikranth Srivatsa <rama22446@gmail.com>
2019-12-06 21:41:12 -0800

    fix output

--- calculate_score.py
+++ calculate_score.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from student_utils import *
-import utils
+import calc_output
 import multiprocessing as mp
 
 pool = mp.Pool(mp.cpu_count())
@@ -9,6 +9,6 @@ pool = mp.Pool(mp.cpu_count())
 outputs = sorted(os.listdir('phase2_outputs/'))
 estimated_score = 0
 num_inputs = len(outputs)
-results = pool.map(utils.calc_output_file_ratio, outputs)
+results = pool.map(calc_output.calc_output_file_ratio, outputs)
 pool.close()
 print(sum(results)/num_inputs * 100)
